8ee46d02c06533e4e0cc8bf3722e20c644643a20
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-04-16 13:31:16 -0400

    moving mysql settings out of application.properties to mysql.application.properties

--- src/main/java/com/deshmukhamit/springbootmysql/SpringbootMysqlApplication.java
+++ src/main/java/com/deshmukhamit/springbootmysql/SpringbootMysqlApplication.java
@@ -2,8 +2,10 @@ package com.deshmukhamit.springbootmysql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "application.properties","mysql.application.properties" })
 public class SpringbootMysqlApplication {
 
 	public static void main(String[] args) {
