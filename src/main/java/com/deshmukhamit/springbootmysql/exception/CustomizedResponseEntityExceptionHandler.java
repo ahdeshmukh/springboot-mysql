1c84b3ba301c28415d44f4ba5a91b569f045a60e
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-04-17 12:06:35 -0400

    removing status  & errorOccured from error response json, code refactor

--- src/main/java/com/deshmukhamit/springbootmysql/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/deshmukhamit/springbootmysql/exception/CustomizedResponseEntityExceptionHandler.java
@@ -11,32 +11,28 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import javax.persistence.PersistenceException;
 
-//import com.deshmukhamit.springbootmysql.exception.ResourceNotFoundException;
-
 @ControllerAdvice
 @RestController
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
-        return new ResponseEntity<>(this.getErrorDetails(ex, request, HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(this.getErrorDetails(ex, request), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        return new ResponseEntity<>(this.getErrorDetails(ex, request, HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(this.getErrorDetails(ex, request), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(PersistenceException.class)
     public final ResponseEntity<ErrorDetails> handlePersistenceException(PersistenceException ex, WebRequest request) {
-        return new ResponseEntity<>(this.getErrorDetails(ex, request, HttpStatus.BAD_GATEWAY), HttpStatus.BAD_GATEWAY);
+        return new ResponseEntity<>(this.getErrorDetails(ex, request), HttpStatus.BAD_GATEWAY);
     }
 
-    private ErrorDetails getErrorDetails(Exception ex, WebRequest request, HttpStatus status) {
-        ErrorDetails errorDetails = new ErrorDetails(ex.getMessage(),
-                request.getDescription(false), status.toString());
-
-        return errorDetails;
+    private ErrorDetails getErrorDetails(Exception ex, WebRequest request) {
+        return new ErrorDetails(ex.getMessage(),
+                request.getDescription(false));
     }
 
 }
--- src/main/java/com/deshmukhamit/springbootmysql/exception/ErrorDetails.java
+++ src/main/java/com/deshmukhamit/springbootmysql/exception/ErrorDetails.java
@@ -1,24 +1,14 @@
 package com.deshmukhamit.springbootmysql.exception;
 
-import java.util.Date;
-
 public class ErrorDetails {
-    //private Date timestamp;
     private String message;
     private String details;
-    private String status;
 
-    public ErrorDetails(String message, String details, String status) {
+    ErrorDetails(String message, String details) {
         super();
-        //this.timestamp = timestamp;
         this.message = message;
         this.details = details;
-        this.status = status;
     }
-
-    /*public Date getTimestamp() {
-        return timestamp;
-    }*/
 
     public String getMessage() {
         return message;
@@ -26,14 +16,6 @@ public class ErrorDetails {
 
     public String getDetails() {
         return details;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public Boolean getErrorOccured() {
-        return true;
     }
 
 }
