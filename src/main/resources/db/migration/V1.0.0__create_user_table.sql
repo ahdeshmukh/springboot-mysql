21d26bf2f3b8e8bd05a0c2045d0354580faaf430
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-05-02 16:00:11 -0400

    creating tables and database migrations using flyway

---
+++ src/main/resources/db/migration/V1.0.0__create_user_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS user;
+
+CREATE TABLE user (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  email varchar(60) NOT NULL,
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  UNIQUE KEY UK_email (email)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/main/resources/db/migration/V1.0.1__user_table_add_password_col.sql
@@ -0,0 +1,1 @@
+ALTER TABLE user ADD COLUMN password VARCHAR(255) NOT NULL AFTER last_name;
