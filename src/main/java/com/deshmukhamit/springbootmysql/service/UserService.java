73d1e87bb869fb9c4fcc7ca33f75b41380a9e5e5
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-05-07 23:04:44 -0400

    removing unused imports

--- src/main/java/com/deshmukhamit/springbootmysql/service/UserService.java
+++ src/main/java/com/deshmukhamit/springbootmysql/service/UserService.java
@@ -7,8 +7,6 @@ import com.deshmukhamit.springbootmysql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
