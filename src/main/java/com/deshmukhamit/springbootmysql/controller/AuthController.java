80939bff1f242b6b112dcd7f46fa13c93d624c63
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-06-18 16:20:14 -0400

    making the login endpoint a GET instead of a POST

--- src/main/java/com/deshmukhamit/springbootmysql/controller/AuthController.java
+++ src/main/java/com/deshmukhamit/springbootmysql/controller/AuthController.java
@@ -3,7 +3,7 @@ package com.deshmukhamit.springbootmysql.controller;
 import com.deshmukhamit.springbootmysql.model.User;
 import com.deshmukhamit.springbootmysql.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class AuthController extends BaseController {
     @Autowired
     private AuthService authService;
 
-    @PostMapping("/login")
+    @GetMapping("/login")
     public User login(@RequestBody Map<String, String> json) {
         String email = json.getOrDefault("email", "");
         String password = json.getOrDefault("password", "");
