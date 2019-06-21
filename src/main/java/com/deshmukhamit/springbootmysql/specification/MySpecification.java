3a4442d2c6da638d53fa18f9d6882052a31c4bdc
Amit Deshmukh <amit.deshmukh.alerts@gmail.com>
2019-06-20 23:46:35 -0400

    adding potentially useful URL for reference

--- src/main/java/com/deshmukhamit/springbootmysql/specification/MySpecification.java
+++ src/main/java/com/deshmukhamit/springbootmysql/specification/MySpecification.java
@@ -1,9 +1,9 @@
 package com.deshmukhamit.springbootmysql.specification;
 
-//import com.deshmukhamit.springbootmysql.model.User;
 import org.springframework.data.jpa.domain.Specification;
 
 // https://stackoverflow.com/questions/39167189/spring-boot-dynamic-query
+// https://spring.io/blog/2011/04/26/advanced-spring-data-jpa-specifications-and-querydsl/
 
 public class MySpecification {
     public static Specification<Object> withEqual(String attribute, Object value) {
